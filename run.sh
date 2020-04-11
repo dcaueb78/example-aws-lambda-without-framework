b65f101631a175ca8922db73c2bad7490485fb89
Cauê Kotarski <caue1017@hotmail.com>
2020-04-11 16:44:42 -0300

    First commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+# 1o passo criar arquivo de politicas de seguranca
+# 2o criar rol de seguranca na AWS
+
+aws iam create-role \ 
+  --role-name lambda-exemplo \
+  --assume-role-policy-document file://politicas.json \
+  | tee logs/role.log
